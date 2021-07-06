bc8244359851888acd9310448fe5b885b843609b
tooba fazal <toobafazal75@gmail.com>
2021-07-06 10:27:56 -0700

    continue

---
+++ app.js
@@ -0,0 +1,8 @@
+function getbtn(number){
+    var input = document.getElementById("result");
+    input.value += number
+}
+function clear(){
+    var input = document.getElementById("result");
+    input.value = " ";
+}
